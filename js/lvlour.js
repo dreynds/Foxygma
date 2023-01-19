72e855ad1ae68fdc4f6550345e0296312fd65c31
Dreyn <72932055+dreynds@users.noreply.github.com>
2023-01-18 21:41:46 -0300

    update on lvl four

--- js/lvlour.js
+++ js/lvlour.js
@@ -4,7 +4,7 @@ var errado = "Você <span><strong>Errou</strong></span>."
 function capturar() {
     var awnser = document.querySelector("#valor").value;
     if (awnser == "Solanum lycopersicum" || awnser == "solanum lycopersicum" || awnser == "SOLANUM LYCOPERSICUM" || awnser == "Solanum Lycopersicum" || awnser == "solanum Lycopersicum") {
-        document.querySelector("#decisao").innerHTML = "<a href='../html/'><button>PROXIMA FASE</button></a>"
+        document.querySelector("#decisao").innerHTML = "<a href='../html/maybeyoushouldputithere.html'><button>PROXIMA FASE</button></a>"
     } else {
         document.querySelector("#decisao").innerHTML = errado
     }
